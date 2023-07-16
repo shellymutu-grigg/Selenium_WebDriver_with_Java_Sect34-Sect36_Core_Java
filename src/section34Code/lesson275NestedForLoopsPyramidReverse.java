eb91ea94f9c3a787147935d117e533ae10a7b705
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-07-17 08:33:25 +1200

    Section 34 Code update for lesson 276

---
+++ src/section34Code/lesson275NestedForLoopsPyramidReverse.java
@@ -0,0 +1,20 @@
+package section34Code;
+
+import java.text.MessageFormat;
+
+public class lesson275NestedForLoopsPyramidReverse {
+
+	public static void main(String[] args) {
+
+		// First implementation
+		int counter = 1;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(MessageFormat.format("{0} ", counter));
+				counter++;
+			}
+			System.out.println();
+		}
+
+	}
+}
