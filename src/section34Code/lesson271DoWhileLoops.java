44d059ef98708cd599b118a22014a5d7abf64736
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-07-17 07:53:33 +1200

    Section 34 Code update for lesson 271

--- src/section34Code/lesson271DoWhileLoops.java
+++ src/section34Code/lesson271DoWhileLoops.java
@@ -2,21 +2,20 @@ package section34Code;
 
 import java.text.MessageFormat;
 
-public class lesson270WhileLoops {
+public class lesson271DoWhileLoops {
 
 	public static void main(String[] args) {
 
 		// Initialize variable
-		int i = 10;
+		int i = 20;
 
-		// A while loop will NOT always guarantee one execution
-		while (i > 0) {
+		// A do while loop will always guarantee one execution
+		do {
 			System.out.println(MessageFormat.format("i is {0}", i));
 
 			// Ensure to decrement rather than increment
 			i--;
-		}
 
+		} while (i > 0);
 	}
-
 }
