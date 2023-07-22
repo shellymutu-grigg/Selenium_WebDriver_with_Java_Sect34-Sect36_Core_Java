22a6f6c6f3bc97487f5ae0d43f576d86e12dfe80
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-07-23 07:36:53 +1200

    Section 35 Code update for lesson 294

---
+++ src/section35Code/Lesson294DatClass.java
@@ -0,0 +1,26 @@
+package section35Code;
+
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Lesson294DatClass {
+
+	public static void main(String[] args) {
+
+		Date date = new Date();
+
+		// Print out current date time
+		System.out.println(MessageFormat.format("Current date is: {0}", date.toString()));
+
+		// Print date in specific format
+		SimpleDateFormat newDateFormat = new SimpleDateFormat("dd MM yyyy");
+		System.out.println(MessageFormat.format("Current date in specific format is: {0}", newDateFormat.format(date)));
+
+		// Print date in specific format
+		SimpleDateFormat otherDateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+		System.out.println(
+				MessageFormat.format("Current date in other specific format is: {0}", otherDateFormat.format(date)));
+	}
+
+}
